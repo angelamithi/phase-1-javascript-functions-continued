0177087b0507ff8d09c1a1f237dda481d3f06f7f
+ <+>
2023-09-19 14:07:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun(fun='roller-skate') {
+ return `This Saturday, I want to ${fun}!`;
+}
+saturdayFun("bathe my dog");
+
+function mondayWork(work='go to the office') {
+return `This Monday, I will ${work}.`
+}
+mondayWork("work from home");
+function wrapAdjective(stringName="*") {
+    return function(adjective='special') {
+        return `You are ${stringName}${adjective}${stringName}!`;
+    };
+}
+wrapAdjective("!!!");
